a579a4d9cfa49308f6c4c935b530c83903fc6a44
Marco Biedermann <marco.biedermann@gmx.de>
2016-10-17 21:05:15 +0200

    add "Isotope"

---
+++ ui/layout/isotope-layout/readme.md
@@ -0,0 +1,4 @@
+[![dependencies Status](https://david-dm.org/marcobiedermann/playground/status.svg?path=ui/layout/isotope-layout)](https://david-dm.org/marcobiedermann/playground?path=ui/layout/isotope-layout) [![devDependencies Status](https://david-dm.org/marcobiedermann/playground/dev-status.svg?path=ui/layout/isotope-layout)](https://david-dm.org/marcobiedermann/playground?path=ui/layout/isotope-layout&type=dev)
+
+# Isotope Layout
+
---
+++ ui/layout/isotope-layout/source/assets/js/script.js
@@ -0,0 +1,50 @@
+import imagesLoaded from 'imagesloaded';
+import Isotope      from 'isotope-layout';
+
+const isotopeFilterButtons = document.querySelectorAll('.js-isotope-filter-button');
+const isotopeSortButtons   = document.querySelectorAll('.js-isotope-sort-button');
+
+let isotope;
+let filterValue = localStorage.getItem('isotope-filter');
+let sortValue   = localStorage.getItem('isotope-sort');
+
+imagesLoaded('.js-isotope', () => {
+
+  isotope = new Isotope(document.querySelector('.js-isotope-layout'), {
+    itemSelector: '.js-isotope-item',
+    filter      : filterValue,
+    sortBy      : sortValue,
+    getSortData : {
+      'js-isotope-sort-age' : '.js-isotope-sort-age',
+      'js-isotope-sort-name': '.js-isotope-sort-name'
+    }
+  });
+
+});
+
+Array.from(isotopeFilterButtons).map((isotopeFilterButton) => {
+  return isotopeFilterButton.addEventListener('click', () => {
+    filterValue = isotopeFilterButton.getAttribute('data-filter');
+
+    localStorage.setItem('isotope-filter', filterValue)
+
+    isotope.arrange({
+      filter: filterValue
+    });
+
+  });
+});
+
+Array.from(isotopeSortButtons).map((isotopeSortButton) => {
+  return isotopeSortButton.addEventListener('click', () => {
+    sortValue = isotopeSortButton.getAttribute('data-sort');
+
+    localStorage.setItem('isotope-sort', sortValue)
+
+    isotope.arrange({
+      sortBy: sortValue
+    });
+
+  });
+
+});
