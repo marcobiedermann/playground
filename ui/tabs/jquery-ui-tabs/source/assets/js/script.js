6a8259cd11850621411eec842c076ef97c4af398
Marco Biedermann <marco.biedermann@gmx.de>
2016-10-24 20:27:57 +0200

    get and set active tab from and to window location hash

--- ui/tabs/jquery-ui-tabs/source/assets/js/script.js
+++ ui/tabs/jquery-ui-tabs/source/assets/js/script.js
@@ -1,4 +1,11 @@
 import jQuery from 'jquery';
 import 'jquery-ui-browserify';
 
-$('.js-ui-tabs').tabs();
+$('.js-ui-tabs').tabs({
+  activate: (event, ui) => {
+    window.location.hash = ui.newPanel.attr('id');
+  },
+  select: (event, ui) => {
+    window.location.hash = ui.tab.hash;
+  }
+});
