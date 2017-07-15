04befac23ececf4fbc1269f5d9f54bfa6b867a9f
Marco Biedermann <marco.biedermann@gmx.de>
2017-07-15 13:28:52 +0200

    get and set accordion hash

--- ui/accordion/jquery-ui-accordion/source/assets/js/script.js
+++ ui/accordion/jquery-ui-accordion/source/assets/js/script.js
@@ -1,19 +1,35 @@
 import jQuery from 'jquery';
 import 'jquery-ui-browserify';
 
-$('.js-ui-accordion').accordion({
+const $uiAccordion = $('.js-ui-accordion');
+
+$uiAccordion.accordion({
+  collapsible: true,
+  heightStyle: 'content',
+
   activate: (event, ui) => {
     const newHeaderId = ui.newHeader.attr('id');
 
     if (newHeaderId) {
-      window.location.hash = newHeaderId;
+      history.pushState(null, null, `#${newHeaderId}`);
     }
   },
-  create: function(event, ui) {
-    const $this = $(this);
 
-    $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($(window.location.hash)));
-  },
-  collapsible: true,
-  heightStyle: 'content'
+  create: (event, ui) => {
+    const $this            = $(event.target);
+    const $activeAccordion = $(window.location.hash);
+
+    if ($this.find($activeAccordion).length) {
+      $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+    }
+  }
+});
+
+$(window).on('hashchange', event => {
+  const $activeAccordion = $(window.location.hash);
+  const $parentAccordion = $activeAccordion.parents('.js-ui-accordion')
+
+  if ($activeAccordion.length) {
+    $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
+  }
 });
