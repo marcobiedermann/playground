9376158d121b163372e91631f27546ee822159d1
Marco Biedermann <marco.biedermann@gmx.de>
2017-08-18 22:15:43 +0200

    move randomInteger function into own file

--- ui/chart/d3/d3-bar-chart/source/assets/js/generate-data.js
+++ ui/chart/d3/d3-bar-chart/source/assets/js/generate-data.js
@@ -1,18 +1,16 @@
+import randomInteger from './random-integer';
+
 function generateData(n) {
   const data = [];
 
   while (n--) {
     data.push({
       date: new Date(Date.now() - (n * 1000 * 60 * 60 * 24)),
-      value: generateRandomInteger(0, 12)
+      value: randomInteger(0, 12)
     });
   }
 
   return data;
-}
-
-function generateRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default generateData;
