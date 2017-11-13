888db2305995ccd1524d65417698be499923c5ef
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-13 23:57:54 +0100

    add is-loading class to body

--- ui/layout/masonry-layout/source/assets/js/script.js
+++ ui/layout/masonry-layout/source/assets/js/script.js
@@ -1,8 +1,10 @@
 import imagesLoaded from 'imagesloaded';
-import Masonry      from 'masonry-layout';
+import Masonry from 'masonry-layout';
 
 imagesLoaded('.js-images-loaded', () => {
-  new Masonry(document.querySelector('.js-masonry'), {
-    itemSelector: '.js-masonry-item'
+  const masonry = new Masonry(document.querySelector('.js-masonry'), {
+    itemSelector: '.js-masonry-item',
   });
+
+  document.body.classList.remove('is-loading');
 });
